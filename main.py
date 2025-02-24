54a50a6133d48168580bcadf1921ecc708c9c00f
ihawkeye <ogabrielmacedo.dev@gmail.com>
2025-02-24 15:41:15 -0300

    commit teste ignore

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 print("teste")
+
+def soma (a,b):
+    resultado = a+b
+    print(resultado)
+
+def subtrai (a,b):
+    resultado = a-b
+    print(resultado)
+
+def multiplica (a,b):
+    resultado = a*b
+    print(resultado)
+
+def divide (a,b):
+    resultado = a/b
+    print(resultado)
+
+soma(4,5)
+subtrai (10,4)
+multiplica (8,7)
+divide (9,3)
